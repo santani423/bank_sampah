31600958615dc163cd8e2cc8a6db1c081945d755
santani423 <santani423@gmail.com>
2025-10-29 22:04:19 +0700

    penyesuaian perhitunga effektive rate

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::apiResource('activities', ActivityController::class);
 
 Route::post('/api/user-face/create', [UserFaceController::class, 'create'])
     ->name('api.user-face.create')
-    ->middleware('throttle:6000,1'); // batasi max 60 request per menit
+    ->middleware('throttle:60,1'); // batasi max 60 request per menit
 
 
 
