e5d1325e120d6ced67c01e529294448bb656a118
santani423 <santani423@gmail.com>
2025-10-30 01:04:44 +0700

    penyesuaian database

--- resources/views/pages/admin/activities/show.blade.php
+++ resources/views/pages/admin/activities/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Detail Activity')
 
 @push('style')
-    <style>
+    {{-- <style>
         /* Card untuk activity */
         .activity-card {
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
@@ -119,7 +119,7 @@
             border: none;
             border-radius: 10px;
         }
-    </style>
+    </style> --}}
 @endpush
 
 @section('main')
