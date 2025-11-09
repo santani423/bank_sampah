7993e4b241bce102435d07a43b701ab302c80b1d
santani423 <santani423@gmail.com>
2025-11-09 14:07:10 +0700

    sdf

--- resources/views/layouts/menu/petugas.blade.php
+++ resources/views/layouts/menu/petugas.blade.php
@@ -38,10 +38,10 @@
                  <a href="{{ route('petugas.transaksi.index') }}" class="sidebar-submenu__link">
                      Transaksi Setoran </a>
              </li>
-             <li class="sidebar-submenu__item">
+             {{-- <li class="sidebar-submenu__item">
                  <a href="{{ route('petugas.transaksi.top-up') }}" class="sidebar-submenu__link">
                      Top Up </a>
-             </li>
+             </li> --}}
              <li class="sidebar-submenu__item">
                  <a href="{{ route('petugas.pengiriman.index') }}" class="sidebar-submenu__link">
                      Pengiriman Sampah </a>
