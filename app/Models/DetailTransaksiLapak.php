60912e0bac5f92082a4bdafb0a243be899f3cc9f
santani423 <santani423@gmail.com>
2025-12-14 01:47:54 +0700

    penyesuaian layout form inp0ut

---
+++ app/Models/DetailTransaksiLapak.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailTransaksiLapak extends Model
+{
+    use HasFactory;
+
+    protected $table = 'detail_transaksi_lapak';
+
+    /**
+     * Relasi ke model Sampah
+     */
+    public function sampah()
+    {
+        return $this->belongsTo(Sampah::class, 'sampah_id');
+    }
+}
