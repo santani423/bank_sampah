91cb24d02c69fc887d8ba712433eec7614d8f84b
santani423 <santani423@gmail.com>
2026-01-10 21:27:58 +0700

    add model cabang

---
+++ app/Models/Cabang888.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\LogsActivity;
+
+class Cabang extends Model
+{
+    use HasFactory, LogsActivity;
+    protected $guarded = [];
+
+     
+    public function lapaks()
+    {
+        return $this->hasMany(Lapak::class, 'cabang_id');
+    }
+
+    /**
+     * Relasi ke tabel petugas melalui petugas_cabangs
+     * Satu cabang bisa memiliki banyak petugas
+     */
+    public function petugas()
+    {
+        return $this->belongsToMany(Petugas::class, 'petugas_cabangs', 'cabang_id', 'petugas_id');
+    }
+}
