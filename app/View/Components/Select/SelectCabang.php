8e6514b20caff25064365a5341f423157dc69bd2
santani423 <santani423@gmail.com>
2026-01-10 17:45:19 +0700

    l

--- app/View/Components/Select/SelectCabang.php
+++ app/View/Components/Select/SelectCabang.php
@@ -10,9 +10,7 @@ use Illuminate\View\Component;
 
 class SelectCabang extends Component
 {
-    /**
-     * Create a new component instance.
-     */
+     
     public $cabang, $name;
     public function __construct($name = 'cabang')
     {
