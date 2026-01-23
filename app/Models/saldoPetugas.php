2856409d8a6e34db3149a0f3bd955722861f4572
santani423 <santani423@gmail.com>
2026-01-24 02:10:46 +0700

    pesan commit

--- app/Models/saldoPetugas.php
+++ app/Models/saldoPetugas.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\LogsActivity;
 
-class saldoPetugas extends Model
+class SaldoPetugas extends Model
 {
     use HasFactory, LogsActivity;
     protected $fillable = [
