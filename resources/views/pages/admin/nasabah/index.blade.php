b541282d525a13e9d1a48b94b50b4097dc5b2d24
santani423 <santani423@gmail.com>
2026-01-23 21:47:05 +0700

    pesan commit

--- resources/views/pages/admin/nasabah/index.blade.php
+++ resources/views/pages/admin/nasabah/index.blade.php
@@ -160,15 +160,15 @@
                 .then(res => {
                     
                     if (res?.success) {
-                        console.log('nasabah',res);
-                        // current_page = res?.pagination.current_page;
-                        // totalPages = res?.pagination.last_page;
-                        // renderTable(res?.data, res?.pagination);
-                        // renderPagination(res?.pagination);
-
-                        // spinner.style.display = 'none';
-                        // table.style.display = 'table';
-                        // pagination.style.display = 'flex';
+                        current_page = res?.pagination.current_page;
+                        totalPages = res?.pagination.last_page;
+                        renderTable(res?.data, res?.pagination);
+                        renderPagination(res?.pagination);
+                        
+                        spinner.style.display = 'none';
+                        table.style.display = 'table';
+                        pagination.style.display = 'flex';
+                        console.log('nasabahiiii44',res);
                     }
                 })
                 .catch(() => {
