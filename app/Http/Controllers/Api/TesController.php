41ea7f62e913094fa25e03f151f78568b8f3d72d
santani423 <santani423@gmail.com>
2026-01-30 21:15:53 +0700

    ok

--- app/Http/Controllers/Api/TesController.php
+++ app/Http/Controllers/Api/TesController.php
@@ -17,7 +17,14 @@ class TesController extends Controller
         $data->save();
         $external_id = $request->transaction['external_id'] ?? '';
 
-        $pencairanLapak = PencairanLapak::with('lapak')->where('kode_pencairan', $external_id)->first();
+        $pencairanLapak = PencairanLapak::with('lapak.jenisMetodePenarikan')->where('kode_pencairan', $external_id)->first();
+
+        if ($pencairanLapak) {
+           if($pencairanLapak->sumber_dana == 'saldo_admin'){
+               
+                
+           }
+        }   
 
         return response()->json([
             'message' => 'Tes API works!',
