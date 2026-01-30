f5f340543a060ac011e4f07c4be31aef3cf4b3e0
santani423 <santani423@gmail.com>
2026-01-30 21:47:52 +0700

    ok

--- app/Http/Controllers/Api/TesController.php
+++ app/Http/Controllers/Api/TesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\PencairanLapak;
+use App\Models\SaldoUtama;
 use App\Models\Tess;
 use Illuminate\Http\Request;
 
@@ -20,11 +21,15 @@ class TesController extends Controller
         $pencairanLapak = PencairanLapak::with('lapak.jenisMetodePenarikan')->where('kode_pencairan', $external_id)->first();
 
         if ($pencairanLapak) {
-           if($pencairanLapak->sumber_dana == 'saldo_admin'){
-               
-                
-           }
-        }   
+            if ($pencairanLapak->sumber_dana == 'saldo_admin') {
+
+                $saldoUtama = SaldoUtama::first();
+                if ($saldoUtama && $saldoUtama->saldo >= $pencairanLapak->jumlah_pencairan) { 
+                    $saldoUtama->saldo -= $pencairanLapak->jumlah_pencairan;
+                    $saldoUtama->save(); 
+                } 
+            }
+        }
 
         return response()->json([
             'message' => 'Tes API works!',
