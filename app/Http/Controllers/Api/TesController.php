882c27a8b81d18061823feb1ea34c83e3827624f
santani423 <santani423@gmail.com>
2026-01-30 20:42:14 +0700

    ok

--- app/Http/Controllers/Api/TesController.php
+++ app/Http/Controllers/Api/TesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\PencairanLapak;
 use App\Models\Tess;
 use Illuminate\Http\Request;
 
@@ -15,13 +14,6 @@ class TesController extends Controller
         $data->name = 'disbursementSend';
         $data->description = json_encode($request->all());
         $data->save();
-
-        $transak = PencairanLapak::where('kode_pencairan', $request->transaction->external_id)->first();
-        // if ($transak) {
-        //     $transak->status = $request->transaction->status;
-        //     $transak->save();
-        // }
-        
 
         return response()->json([
             'message' => 'Tes API works!',
